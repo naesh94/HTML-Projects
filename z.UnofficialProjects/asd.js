13fef5840df0881154f44740e311e7d2b9a29e06
naesh94 <sheanv94@gmail.com>
2023-12-02 07:19:00 +0800

    ....

---
+++ z.UnofficialProjects/asd.js
@@ -0,0 +1,19 @@
+
+// Setup
+let sum = 0;
+
+function addThree() {
+  sum = sum + 3; 
+  console.log(sum)
+}
+
+// Only change code below this line
+function addFive(){
+  sum = sum + 5; 
+  console.log(sum)
+}
+
+// Only change code above this line
+
+addThree();
+addFive();
